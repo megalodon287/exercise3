67dbfc882b5a40b1a2bda2a2fd6aacad2df13bff
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-20 21:56:28 +0800

    Create photo2.php

---
+++ photo2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Photo Two</title>
+</head>
+<style>
+
+div.c {
+  line-height:.02px;
+}
+
+</style>
+
+<body style="background-color:#FFA07A;font-family:Courier New;">
+  
+<br>
+<br>
+
+<div class="c">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:	#FA8072;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:white;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#20B2AA;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+</div>
+
+<br>
+<br>
+
+<center><img src="https://steamuserimages-a.akamaihd.net/ugc/927045220124735214/FFA72B10E64E5F120D9BD98D8FEE05C292AA03AC/" style="width:552px;height:393px;"></center>
+
+<br>
+
+<p style="text-align:center;font-size:20px;color:black;"><a href="homepage.php"><i>back to home</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="photo3.php"><i>next photo</i></a></p>
+ <p style="text-align:center;font-size:20px;color:black;"><a href="index.php"><i>log out</i></a></p>
+
+</body>
+</html>
+
+
+
