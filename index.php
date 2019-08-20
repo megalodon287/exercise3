8e74469960c50e8ebd3f133b63c7f5f33275facd
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-20 21:52:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>LOGIN PAGE</title>
+
+</head>
+
+
+<body style="background-color:#FFA07A;font-family:Courier New;color:white;">
+
+
+
+ <center><h1 style="color:white;font-size:90px;"><i>l&nbspo&nbspg&nbspi&nbspn</i></h1>
+
+
+
+ <form action="/action_page.php">
+  <i>username:</i>&nbsp<input type="text" name="Username">
+  <br/>
+  <br/>
+  <br/>
+ <i>password:</i>&nbsp<input type="text" name="Password">
+  <br/>
+  <br/>
+  <br/>
+</form>
+
+
+
+<a href="homepage.php"><input type="submit" value="LOG IN"></a>
+
+<br/>
+ <br/>
+<br/>
+<a href="register.php"><input type="submit" value="REGISTER HERE"></a>
+
+
+
+
+
+</body>
+
+</html>
