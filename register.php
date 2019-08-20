1cfebdfe186e760dd1b974c2b7e20a2b2d1ca513
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-20 21:53:34 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>LOGIN PAGE</title>
+
+</head>
+
+
+<body style="background-color:#FFA07A;font-family:Courier New;color:white;">
+
+
+
+ <center><h1 style="color:white;font-size:60px;"><i>r&nbspe&nbspg&nbspi&nbsps&nbspt&nbspe&nbspr</i></h1>
+
+
+
+ 
+ <form action="/action_page.php">
+<i>email address:</i>&nbsp<input type="text" name="Email Address">
+<br/>
+<br/>
+  <br/>
+<i>confirm email:</i>&nbsp<input type="text" name="Email Address">
+<br/>
+<br/>
+  <br/>
+<i>password:</i>&nbsp<input type="text" name="Password">
+<br/>
+<br/>
+  <br/>
+<i>confirm password:</i>&nbsp<input type="text" name="Password">
+  <br/>
+  <br/>
+    <br/>
+ 
+<a href="homepage.php"><input type="submit" value="REGISTER"></a>
+
+</form>
+   
+
+</body>
+
+</html>
+
