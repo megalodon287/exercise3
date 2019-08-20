4d09916fad7f4e17c7e2c6e4521677d4b11bf72c
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-20 21:55:04 +0800

    Create photo1.php

---
+++ photo1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Photo One</title>
+</head>
+<style>
+
+div.c {
+  line-height:.02px;
+}
+
+</style>
+
+<body style="background-color:#FFA07A;font-family:Courier New;">
+<br>
+<br>
+
+<div class="c">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:	#FA8072;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:white;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#20B2AA;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+</div>
+
+<br>
+<br>
+
+<center><img src="https://66.media.tumblr.com/2daa672ed482d2d61a7f6e907bcf3f6a/tumblr_orcsr0qHOY1vegc5oo1_640.gif" style="width:552px;height:393px;"></center>
+
+<br>
+
+<p style="text-align:center;font-size:20px;color:black;"><a href="homepage.php"><i>back to home</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="photo2.php"><i>next photo</i></a></p>
+ <p style="text-align:center;font-size:20px;color:black;"><a href="index.php"><i>log out</i></a></p>
+
+
+</body>
+</html>
+
+
