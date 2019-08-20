4d20c1c5c90376d0946c69c1b7fc05359ee586ef
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-20 21:52:37 +0800

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Homepage</title>
+</head>
+<style>
+
+div.c {
+  line-height:.02px;
+}
+
+</style>
+
+<body style="background-color:#FFA07A;font-family:Courier New;">
+<br>
+<br>
+
+<div class="c">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:	#FA8072;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:white;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#20B2AA;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+</div>
+<br>
+
+<h1 style="text-align:center;font-size:33px;color:black;"><i>l&nbspi&nbsps&nbspt&nbsp&nbsp&nbspo&nbsp&nbspf&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbsps</i></h1>
+
+
+<p style="text-align:center;font-size:20px;color:black;">
+<i><a href="photo1.php">photo one</a></i></p>
+<p style="text-align:center;font-size:20px;color:black;">
+<i><a href="photo2.php">photo two</a></i></p>
+<p style="text-align:center;font-size:20px;color:black;"><i><a href="photo3.php">photo three</a></i></p>
+
+<br/>
+<br/>
+<br/>
+
+<center><a href="index.php"><i>log out</i></a></center>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
